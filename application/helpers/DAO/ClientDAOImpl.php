d807d95e51813f23d1608fc45bbf1e1d63a6d089
yazdani.mohammad106 <yazdani.mohammad106@yahoo.com>
2017-05-04 15:49:56 -0400

    client data access object created

--- application/helpers/DAO/ClientDAOImpl.php
+++ application/helpers/DAO/ClientDAOImpl.php
@@ -7,6 +7,7 @@
  */
 
 
+// TODO : DOCS
 
 namespace application\helpers\DAO;
 
@@ -26,13 +27,15 @@ class ClientDAOImpl extends DAOImpl implements ClientDAO
     public function save(Client $client)
     {
         // The object to be saved is the parameter $client of type "model/Client"
-        try {
+        try 
+				{
             // TODO : DOC
             $this->em->persist($client);
             // TODO : DOC
             $this->em->flush();
         } // TODO : DOC
-        catch (\Exception $exception) {
+        catch (\Exception $exception)
+				{
             die($exception->getMessage());
         }
         // Method returns true so that the caller code can determine the success of operation.
@@ -42,28 +45,61 @@ class ClientDAOImpl extends DAOImpl implements ClientDAO
 
     public function get($id)
     {
-        // TODO: Implement get() method.
-        
+      // TODO: Implement get() method.
+      $client = null;  
+			try 
+			{
+					$client = $this->em->getRepository('client')->find($id);	
+			}
+			catch (...) 
+			{
+			
+			}
+			return $client;
     }
 
     public function delete(Client $client)
     {
-        // TODO: Implement delete() method.
+      // TODO : Implement delete() method.
+			// TODO : Handle FAILURE
+			$this->em->delete($client);
+			$this->em->flush();
     }
     
   
 	public function getByDateCreated($date : DateTime) : array
   {
-      
+		try 
+		{
+			$this->em->findBy(array('dateCreated' => $date));
+		}
+		catch (...)
+		{
+			
+		}
   }
 
 	public function getByDateModified($date : DateTime) : array
   {
-    
+    try 
+		{
+			$this->em->findBy(array('dateModified' => $date));
+		}
+		catch (...)
+		{
+			
+		}
   }
   
 	public function getByJSON($json : string) : \Model
   {
-    
+    try 
+		{
+			$this->em->findBy(array('JSON' => $json));
+		}
+		catch (...)
+		{
+			
+		}
   }
 }
