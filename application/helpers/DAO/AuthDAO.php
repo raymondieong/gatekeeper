cf7d0a6675fc4a47cf5200106d97783c9bb31a5e
Mohammad Yazdani <myazdani@OGAM.local>
2017-05-12 16:30:27 -0400

    dao init:

---
+++ application/helpers/DAO/AuthDAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: myazdani
+ * Date: 5/12/2017
+ * Time: 4:18 PM
+ */
+
+namespace DAO;
+
+
+interface AuthDAO
+{
+    // TODO : Invariant revision
+}
---
+++ application/helpers/DAO/UserDAO.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: myazdani
+ * Date: 5/12/2017
+ * Time: 4:17 PM
+ */
+
+namespace DAO;
+
+
+interface UserDAO extends \DAO
+{
+    // TODO : Invariant revision
+}
